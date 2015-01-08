cec1cfae1c7cdb37fe3b6970c35b0cfb470f9546
Yuki Tetsuka <tetsuka@cheekit.com>
2015-01-09 04:21:02 +0900

    [ホーム画面]クリックイベント追加 [検索方法選択画面]viewのみ作成

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,10 @@
-function doClick(e) {
-    alert($.label.text);
+function openRetrievalMethod(){
+  var view = Alloy.createController('retrievalMethod').getView();
+  $.index.add(view);
+}
+
+function openWriteWin(){
+  Ti.API.debug('openWriteWin');
 }
 
 $.index.open();
