58ea75b50f946a5f91afe92509bfc9a3f43683c5
Yuki Tetsuka <tetsuka@cheekit.com>
2015-01-09 11:41:57 +0900

    [作業で探す画面]viewのみ作成

--- app/controllers/retrievalMethod.js
+++ app/controllers/retrievalMethod.js
@@ -1,1 +1,12 @@
 var args = arguments[0] || {};
+
+function openWorkSearch(){
+  var view = Alloy.createController('workSearch').getView();
+
+  Alloy.Globals.parent.add(view);
+  Alloy.Globals.viewStack.push(view);
+}
+
+function openDateSearch(){
+  Ti.API.debug('openDateSearch');
+}
---
+++ app/controllers/workSearch.js
@@ -0,0 +1,10 @@
+var args = arguments[0] || {};
+
+function transformFunction(model) {
+  // var transform = model.toJSON();
+  // return transform;
+  return model;
+}
+
+// var works = Alloy.Collections.work;
+// works.fetch();
